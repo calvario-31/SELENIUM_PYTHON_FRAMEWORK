895fa84c54e2c78b91378c2244820893f5f8c926
calvario31 <cvargasr.31@gmail.com>
2021-09-12 18:34:20 -0500

    update log info

--- utilities/log_manager.py
+++ utilities/log_manager.py
@@ -55,6 +55,6 @@ def get_logger():
     logger.addHandler(file_handler)
     logger.addHandler(stream_handler)
 
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.INFO)
 
     return logger
